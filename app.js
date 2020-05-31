9acda5aa98975c0d20fd7e347ccfe45cceb85c29
Styku <brtstyczen@gmail.com>
2020-06-01 00:18:53 +0200

    minimal working example

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ new Vue({
         search_string: "",
         params: []
     },
+    methods: {
+        sendStlRequest() {
+            postData = {}
+            this.params.forEach(param => {
+                postData[param.name] = param.value;
+            });
+            this.$http
+                .post("http://127.0.0.1:5000/stl", postData, {responseType: 'arraybuffer'})
+                .then(response => {
+                    var headers = response.headers;
+                    var blob = new Blob([response.data],{type:headers['content-type']});
+                    var link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(blob);
+                    link.download = "keychain.stl";
+                    link.click();
+                });
+        }
+    },
     created: function() {
         this.$http.get('http://127.0.0.1:5000/script/keychain')
             .then(res => {
