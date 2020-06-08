f9fd9578c0f042b9b733a33b7cf7373f8940ef02
Styku <brtstyczen@gmail.com>
2020-06-08 13:41:59 +0200

    show script on the inputs view

--- app.js
+++ app.js
@@ -57,41 +57,59 @@ Vue.component('scriptlist', {
     props: ['scripts', 'selected_script'],
     template: `
         <div class="grid-scripts w3-theme-l4" v-if="selected_script">
-            <div v-for="item in scripts" @click.prevent="$parent.loadScript(item.script)" class="w3-card w3-hover-shadow w3-white w3-center">
+            <div v-for="item in scripts" @click.prevent="$emit('selected', item.script)" class="w3-card w3-hover-shadow w3-white w3-center">
                 <div v-bind:class="{'w3-container': true, 'w3-theme': item.script != selected_script.id, 'w3-theme-d4': item.script == selected_script.id}">
                     <h5>{{ item.script }}</h5>
                 </div>
                 <img v-bind:src="item.image" class="thumbnail"> 
             </div>
         </div>
-    `
+    `,
+    methods: {
+        select(script) {
+            ;
+        }
+    }
 });
 
 Vue.component('scriptparam', {
-    props: ['item'],
+    props: ['script', 'image_catalog'],
     template: `
     <div>
-        <label class="w3-text-blue">{{ item.name }}</label>
-        <template v-if="item.allowed">
-            <select v-model="item.value" class="w3-input w3-border">
-                <option v-for="option in item.allowed" v-bind:value="option">{{ option }}</option>
-            </select>
-        </template>
-        <template v-else-if="item.type === 'category'">
-            <select v-model="item.value" class="w3-input w3-border" @change="$parent.script.selected_category = item.value">
-                <option v-for="(value, key) in $parent.image_catalog" v-bind:value="key">{{ key }}</option>
-            </select>
-        </template>
-        <template v-else-if="item.type === 'image'">
-            <select v-model="item.value" class="w3-input w3-border">
-                <option v-for="image in $parent.image_catalog[$parent.script.selected_category]" v-bind:value="image">{{ image }}</option>
-            </select>
-        </template>
-        <template v-else>
-            <input v-if="item.type === 'string'" type="text" v-model="item.value" class="w3-input w3-border">
-            <input v-else type="number" v-model="item.value" class="w3-input w3-border">
-        </template>
-        <p>{{ item.description }}</p>
+        <div class="title">
+            <h2>{{ script.name}}</h2>
+            <p>{{ script.description }}</p>
+        </div>
+        <div class="grid-inputs">
+            <div>
+                <div v-for="item in script.params" v-bind:item="item">                
+                    <label class="w3-text-blue">{{ item.name }}</label>
+                    <template v-if="item.allowed">
+                        <select v-model="item.value" class="w3-input w3-border">
+                            <option v-for="option in item.allowed" v-bind:value="option">{{ option }}</option>
+                        </select>
+                    </template>
+                    <template v-else-if="item.type === 'category'">
+                        <select v-model="item.value" class="w3-input w3-border" @change="script.selected_category = item.value">
+                            <option v-for="(value, key) in image_catalog" v-bind:value="key">{{ key }}</option>
+                        </select>
+                    </template>
+                    <template v-else-if="item.type === 'image'">
+                        <select v-model="item.value" class="w3-input w3-border">
+                            <option v-for="image in image_catalog[script.selected_category]" v-bind:value="image">{{ image }}</option>
+                        </select>
+                    </template>
+                    <template v-else>
+                        <input v-if="item.type === 'string'" type="text" v-model="item.value" class="w3-input w3-border">
+                        <input v-else type="number" v-model="item.value" class="w3-input w3-border">
+                    </template>
+                    <p>{{ item.description }}</p>
+                </div>
+            </div>
+            <div v-bind:class="{'preview-container-loading': script.preview_loading, 'preview-container': !script.preview_loading}">
+                <img v-bind:src="script.preview" v-if="!script.preview_loading"> 
+            </div>
+        </div>
     </div>
     `
 });
