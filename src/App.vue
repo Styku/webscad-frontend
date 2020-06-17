a2840f9c93e35e4f3bb81ccb940995354fbdaa4c
Styku <brtstyczen@gmail.com>
2020-06-17 12:51:02 +0200

    abstract whole main panel into ScriptView component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
   <main id="app">
-    <div class="grid-main" >
+    <div class="grid-main">
       <div class="left-panel">
-          <menu class="w3-theme-d4">
-              <input v-model="script_filter" class="w3-input w3-border" placeholder="Search...">
-          </menu>
-          <script-list v-bind:scripts="filtered_scripts" v-bind:selected_script="script" @selected="loadScript($event)"></script-list>
+        <menu class="w3-theme-d4">
+          <input v-model="script_filter" class="w3-input w3-border" placeholder="Search..." />
+        </menu>
+        <script-list
+          v-bind:scripts="filtered_scripts"
+          v-bind:selected_script="script"
+          @selected="loadScript($event)"
+        ></script-list>
       </div>
-      <div>
-          <menu class="w3-theme-d4">
-              <button @click.prevent="getPreview(script)" class="w3-button w3-blue">Preview</button>
-              <button @click.prevent="getStl(script)" class="w3-button w3-green">Render</button>
-          </menu>
-          <script-input v-bind:script="script" v-bind:image_catalog="image_catalog" @changed="getPreview(script)"></script-input>
-      </div>
+      <script-view v-bind:script="script" v-bind:image_catalog="image_catalog"></script-view>
     </div>
   </main>
 </template>
 
 <script>
-import ScriptInput from './components/scriptinput.vue'
-import ScriptList from './components/scriptlist.vue'
+import ScriptView from "./components/ScriptView.vue";
+import ScriptList from "./components/scriptlist.vue";
 import { Script, FontawesomeImage } from "./script";
 import { BACKEND_URL } from "./host";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ScriptInput,
+    ScriptView,
     ScriptList
   },
   data() {
@@ -37,57 +35,49 @@ export default {
       image_catalog: null,
       selected_category: null,
       script: null
-    }
+    };
   },
   methods: {
-      getStl(script) {
-          script.getStl();
-      },
-      getPreview(script) {
-          script.getPreviev();
-      },
-      loadScript(script_id) {
-          this.$http.get(BACKEND_URL + "/script/" + script_id)
-          .then(res => {
-              this.script = new Script(res.body, script_id, this.$http);
-              this.script.getPreviev(this.$http);
-          });
-      }
+    loadScript(script_id) {
+      this.$http.get(BACKEND_URL + "/script/" + script_id).then(res => {
+        this.script = new Script(res.body, script_id, this.$http);
+        this.script.getPreviev(this.$http);
+      });
+    }
   },
   created: function() {
-      this.$http.get(BACKEND_URL + "/images")
-      .then(res => {
-          this.image_catalog = res.body.images.map(x => new FontawesomeImage(x));
-      });
-      this.$http.get(BACKEND_URL + "/script")
-      .then(res => {
-          this.scripts = res.body;
-          this.loadScript(this.scripts[0].script);
-      });
+    this.$http.get(BACKEND_URL + "/images").then(res => {
+      this.image_catalog = res.body.images.map(x => new FontawesomeImage(x));
+    });
+    this.$http.get(BACKEND_URL + "/script").then(res => {
+      this.scripts = res.body;
+      this.loadScript(this.scripts[0].script);
+    });
   },
   computed: {
-      filtered_scripts() {
-          return this.scripts.filter( script => {
-              return script.script.toLowerCase().includes(this.script_filter.toLowerCase())
-          });
-      }
+    filtered_scripts() {
+      return this.scripts.filter(script => {
+        return script.script
+          .toLowerCase()
+          .includes(this.script_filter.toLowerCase());
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 main {
-    margin: 0px; 
-    padding: 0px;
-    width: 100%;
-    height: 100%;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 100%;
 }
 
-.grid-main
-{
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    width: 100%;
-    height: 100%;
+.grid-main {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/ScriptView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <menu class="w3-theme-d4">
+      <button @click.prevent="getPreview(script)" class="w3-button w3-blue">Preview</button>
+      <button @click.prevent="getStl(script)" class="w3-button w3-green">Render</button>
+    </menu>
+    <script-input
+      v-bind:script="script"
+      v-bind:image_catalog="image_catalog"
+      @changed="getPreview(script)"
+    ></script-input>
+  </div>
+</template>
+
+<script>
+import ScriptInput from "./scriptinput.vue";
+
+export default {
+  name: "ScriptView",
+  components: {
+    ScriptInput
+  },
+  props: ['script', 'image_catalog'],
+  methods: {
+    getStl(script) {
+      script.getStl();
+    },
+    getPreview(script) {
+      script.getPreviev();
+    }
+  }
+};
+</script>
